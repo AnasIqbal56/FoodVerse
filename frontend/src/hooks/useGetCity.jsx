c71149fc484035bd93036aec8382b0fe6ca74d76
SAWERA-11 <saweraarif2005@gmail.com>
2025-10-21 22:34:00 +0500

    Img & Address

--- frontend/src/hooks/useGetCity.jsx
+++ frontend/src/hooks/useGetCity.jsx
@@ -1,18 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-
-
 import axios from 'axios';
 import { setUserData } from '../redux/userSlice';
 import { serverUrl } from '../App';
 import { use } from 'react';
+import { setCurrentCity,setCurrentAddress,setCurrentState } from '../redux/userSlice';
 
 
 function useGetCity() {
   const dispatch = useDispatch();
 
-  const userData=useSelector(state=>state.user)
+  const {userData}=useSelector(state=>state.user)
   const apiKey=import.meta.env.VITE_GEOAPIKEY;
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(async (position)=>{
@@ -21,7 +20,9 @@ function useGetCity() {
         const result=await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=${apiKey}`)
         dispatch(setCurrentCity(result?.data?.results[0].city))
         dispatch(setCurrentState(result?.data?.results[0].state))
-        dispatch(setCurrentAddress(result?.data?.results[0].address))
+        dispatch(setCurrentAddress(result?.data?.results[0].address_line2 || result?.data?.results[0].address_line1))
+        console.log(result.data);
+        
 
     })
   }, [userData]);
--- frontend/src/pages/CreateEditShop.jsx
+++ frontend/src/pages/CreateEditShop.jsx
@@ -1,12 +1,12 @@
 import React, { use } from "react";
-import { IoMdArrowBack } from "react-icons/io";
+import { IoIosArrowRoundBack } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { FaUtensils } from "react-icons/fa";
 import { useState } from "react";
 
 function CreateEditShop() {
-    const navigate = useNavigate();
+    const navigate = useNavigate(); 
     const { myShopData } = useSelector(state => state.owner);
     const { currentCity, currentState, currentAddress } = useSelector(state => state.user);
 
@@ -14,29 +14,52 @@ function CreateEditShop() {
     const [city,setCity]= useState(myShopData?.city || currentCity)
     const [state,setState]= useState(myShopData?.state || currentState)
     const [address,setAddress]= useState(myShopData?.address || currentAddress)
-
-
+    const [frontendImage,setFrontendImage]= useState(myShopData?.image || null)
+    const [backendImage, setBackendImage]=useState(null)
+    const handleImage = async (e) => {
+        const file = e.target.files[0]
+        setBackendImage(file)
+        setFrontendImage(URL.createObjectURL(file))
+        
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const formData = new FormData()
+            formData.append("name",name)
+            formData.append("city",city)
+            formData.append("state",state)
+            formData.append("address",address)
+            if (backendImage) {
+                formData.append("image",backendImage)   
+            }
+            const result = await axios.post(`${serverUrl}/api/shop/create-edit`,formData,{withCredentials:true})
+        } catch (error) {
+            
+        }
+        
+    }
     return (
         <div className="flex justify-center flex-col items-center p-6 bg-gradient-to-br from-orange-50 relative to-white min-h-screen">
 
             <div className="absolute top-[20px] left-[20px] z-[10] mb-[10px]">
-                <IoMdArrowBack size={35} className="text-[#ff4d2d] cursor-pointer" onClick={() => navigate("/")} />
+                <IoIosArrowRoundBack size={35} className="text-[#ff4d2d] cursor-pointer" onClick={() => navigate("/")} />
             </div>
 
             <div className="max-w-lg w-full bg-white shadow-xl rounded-2xl p-8 border border-orange-100">
 
-                <div className="flex flex-col items-center mb-6">
+                <div className="flex flex-col items-center mb-6" >
                 
-                <div className = "bg-orange-100 p-4 rounded-full mb-4">
+                {/* <div className = "bg-orange-100 p-4 rounded-full mb-4">
                     <FaUtensils className="text-[#ff4d2d] w-16 h-16"/>
                 </div>
 
                 <div className="text 3xl font-extrabold text-gray-900">
                     {myShopData ? "Edit Shop" : "Add Shop"}  
-                </div>
+                </div> */}
                 </div>
 
-                <form className="space-y-5">
+                <form className="space-y-5" onSubmit={handleSubmit}>
                     <div>
                         <label className="block text-sm font-medium text-gray-700 mb-l">Name</label>
                         <input type="text" placeholder="Enter Shop Name" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"       
@@ -46,9 +69,12 @@ function CreateEditShop() {
 
                     <div>
                         <label className="block text-sm font-medium text-gray-700 mb-l">Shop Image</label>
-                        <input type="file" accept="image/*" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500"/>
-                        
-                    </div>
+                        <input type="file" accept="image/*" className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500" onChange= {handleImage} />
+                        {frontendImage && <div className="mt-4">
+                            <img src={frontendImage} alt="" className="w-full h-48 object-cover rounded-lg border" />
+                        </div>
+                   }
+                         </div>
 
                     <div className="grid grid-cols-l md:grid-cols-2 gap-4">
                         <div>
