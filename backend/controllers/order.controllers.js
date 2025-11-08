54c50254e0b9f527d44929006f2eeb4022833fa3
affan-ak-khan <kaffan2019@gmail.com>
2025-11-08 14:01:25 +0500

    OTP verfication succefully sent to GMAIL (Error)

--- backend/controllers/order.controllers.js
+++ backend/controllers/order.controllers.js
@@ -376,7 +376,8 @@ export const sendDeliveryOtp = async (req, res) => {
     shopOrder.deliveryOtp = otp
     shopOrder.otpExpires = Date.now() + 5 * 60 * 1000
     await order.save()
-    await sendDeliveryOtpMail(order.user, otp)
+    await sendDeliveryOtpMail({ user: order.user, otp })
+
     return res.status(200).json({ message: `OTP sent successfully to ${order?.user.fullName}` })
 
   } catch (error) {
